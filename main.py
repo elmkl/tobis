4c17f61e593c432d52c1e854ba609f8506a81763
Elias <moukal@icloud.com>
2026-05-04 14:14:37 +0100

    treat the subfolders as packages

--- main.py
+++ main.py
@@ -7,18 +7,9 @@ from fastapi import FastAPI, Query, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
  
-app = FastAPI(
-    title="tobis",
-    description="API for public transit in Morocco",
-    version="0.1.0",
-)
- 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
+# scaffolding
+app = FastAPI(title="tobis", description="API for public transit in Morocco", version="0.1.0",)
+app.add_middleware(CORSMiddleware, allow_origins=["*"], allow_methods=["*"], allow_headers=["*"],)
  
 # TODO: add routers
  
